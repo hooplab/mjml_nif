645fdd3f70336cded42dfae3394f1f8fcd078ac9
Håvard Lindset <havard@hoopla.no>
2022-02-16 12:22:46 +0100

    upgrade rustler

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Mjml.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.21"},
+      {:rustler, "~> 0.23"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
